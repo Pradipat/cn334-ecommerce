79dd411fa89e442d659e788302aa8c6fa6f9c327
Pradipat <81924877+Pradipat@users.noreply.github.com>
2024-04-29 08:52:28 +0700

    Delete backend directory

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-export const PORT = 8000;
-
-export const mongoDBURL = 
-    'mongodb+srv://cn334pat:Cn334Pradipat!@cn334.yqxhelx.mongodb.net/cn334?retryWrites=true&w=majority&appName=CN334'
--- backend/models/categoriesModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const categoriesSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    subCategories: { type: [String], default: [] },
-  });
-
-export const categories = mongoose.model('Catergory', categoriesSchema);
